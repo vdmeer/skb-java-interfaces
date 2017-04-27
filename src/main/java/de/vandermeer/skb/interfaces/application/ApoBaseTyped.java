f0e9f2273780cb80c19db402389386133e379fe7
vdmeer <sven@vandermeer.de>
2017-04-27 11:56:39 +0100

    finalized documentation

--- src/main/java/de/vandermeer/skb/interfaces/application/ApoBaseTyped.java
+++ src/main/java/de/vandermeer/skb/interfaces/application/ApoBaseTyped.java
@@ -1,14 +1,28 @@
+/* Copyright 2017 Sven van der Meer <vdmeer.sven@mykolab.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.vandermeer.skb.interfaces.application;
 
+/**
+ * A standard typed application options.
+ * 
+ * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
+ * @version    v0.0.1 build 170404 (04-Apr-17) for Java 1.8
+ * @since      v0.0.2
+ */
 public interface ApoBaseTyped<T> extends ApoBase {
-
-//	/**
-//	 * Converts a value of some type to the type the option is using.
-//	 * @param value the value to be converted
-//	 * @return the value converted to the type of the option, null if not possible
-//	 * @throws IllegalStateException if the argument was blank (string) or otherwise problematic
-//	 */
-//	T convertValue(Object value) throws IllegalStateException;
 
 	/**
 	 * Returns the default value of the option.
