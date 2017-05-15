01e2eb31c01fab8d47fabca8c977c06632ac3084
vdmeer <sven@vandermeer.de>
2017-05-15 22:57:43 +0100

    bug fixes

---
+++ src/main/java/de/vandermeer/skb/interfaces/categories/has/HasName.java
@@ -0,0 +1,35 @@
+/* Copyright 2017 Sven van der Meer <vdmeer.sven@mykolab.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.vandermeer.skb.interfaces.categories.has;
+
+import de.vandermeer.skb.interfaces.categories.CategoryHas;
+
+/**
+ * Interface of objects that have a name.
+ *
+ * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
+ * @version    v0.0.2 build 170502 (02-May-17) for Java 1.8
+ * @since      v0.0.3
+ */
+public interface HasName extends CategoryHas {
+
+	/**
+	 * Returns the name of an object.
+	 * @return name
+	 */
+	String getName();
+
+}
