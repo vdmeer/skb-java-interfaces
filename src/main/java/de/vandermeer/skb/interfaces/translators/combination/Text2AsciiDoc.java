e00ee14b82c454b534ed2f6e61d040b68f1fa6a2
vdmeer <sven@vandermeer.de>
2017-05-20 23:01:00 +0100

    adapted to new translator API

---
+++ src/main/java/de/vandermeer/skb/interfaces/translators/combination/Text2AsciiDoc.java
@@ -0,0 +1,66 @@
+/* Copyright 2017 Sven van der Meer <vdmeer.sven@mykolab.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.vandermeer.skb.interfaces.translators.combination;
+
+import de.vandermeer.skb.interfaces.translators.CombinedTranslator;
+
+/**
+ * Combined translator translating from text to AsciiDoc.
+ *
+ * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
+ * @version    v0.0.2 build 170404 (04-Apr-17) for Java 1.8
+ * @since      v0.0.3
+ */
+public interface Text2AsciiDoc extends CombinedTranslator {
+
+	/**
+	 * Returns a new combined translator for text to ASCIIDOC.
+	 * @return new combined translator
+	 */
+	static Text2AsciiDoc create(){
+		return new Text2AsciiDoc() {
+		};
+	}
+
+	@Override
+	default String text2tmp(String input) {
+		return de.vandermeer.skb.interfaces.translators.htmlelement.Text2AsciiDoc.create().text2tmp(input);
+	}
+
+	@Override
+	default String tmp2target(String input) {
+		return de.vandermeer.skb.interfaces.translators.htmlelement.Text2AsciiDoc.create().tmp2target(input);
+	}
+
+	@Override
+	default String translate(String input) {
+		String ret = this.text2tmp(input);
+		ret = this.translateCharacters(ret);
+		ret = this.tmp2target(ret);
+		return ret;
+	}
+
+	@Override
+	default String translateCharacters(String input) {
+		return de.vandermeer.skb.interfaces.translators.character.Text2AsciiDoc.create().translateCharacters(input);
+	}
+
+	@Override
+	default String translateHtmlElements(String input) {
+		return de.vandermeer.skb.interfaces.translators.htmlelement.Text2AsciiDoc.create().translateHtmlElements(input);
+	}
+
+}
---
+++ src/test/java/de/vandermeer/skb/interfaces/translators/character/Test_CodeForDocs.java
@@ -0,0 +1,87 @@
+/* Copyright 2017 Sven van der Meer <vdmeer.sven@mykolab.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.vandermeer.skb.interfaces.translators.character;
+
+import org.junit.Test;
+
+/**
+ * Tests for ASCII Heading for code used in documentation.
+ *
+ * @author     Sven van der Meer &lt;vdmeer.sven@mykolab.com&gt;
+ * @version    v0.0.2 build 170404 (04-Apr-17) for Java 1.8
+ * @since      v0.0.3
+ */
+public class Test_CodeForDocs {
+
+	@Test
+	public void test_LatexWrong(){
+		this.output("LaTeX Wrongs");
+
+		String text = "ä ö <b>ü</b>";
+		System.out.println(de.vandermeer.skb.interfaces.translators.combination.Text2Latex.create().translateCharacters(text));
+		System.out.println(de.vandermeer.skb.interfaces.translators.combination.Text2Latex.create().translate(text));
+
+		this.output();
+	}
+
+	@Test
+	public void test_CharToX(){
+		this.output("char text to X");
+
+		String text = "ä ö ü Š β … € ™ ↔";
+		System.out.println(de.vandermeer.skb.interfaces.translators.character.Text2AsciiDoc.create().translateCharacters(text));
+		System.out.println(de.vandermeer.skb.interfaces.translators.character.Text2Html.create().translateCharacters(text));
+		System.out.println(de.vandermeer.skb.interfaces.translators.character.Text2Latex.create().translateCharacters(text));
+
+		this.output();
+	}
+
+	@Test
+	public void test_FormattingToX(){
+		this.output("formatting text to X");
+
+		String text = "<b>bold</b>, <i>italic</i>, H<sub>2</sub>O, x<sup>y</sup>";
+		System.out.println(de.vandermeer.skb.interfaces.translators.htmlelement.Text2AsciiDoc.create().translateHtmlElements(text));
+		System.out.println(de.vandermeer.skb.interfaces.translators.htmlelement.Text2Html.create().translateHtmlElements(text));
+		System.out.println(de.vandermeer.skb.interfaces.translators.htmlelement.Text2Latex.create().translateHtmlElements(text));
+
+		this.output();
+	}
+
+	@Test
+	public void test_CombinedToX(){
+		this.output("combined text to X");
+
+		String text = "<b>bold ä ö ü</b>, <i>italic Š β …</i>, €<sub>5</sub>O, ™<sup>↔</sup>";
+		System.out.println(de.vandermeer.skb.interfaces.translators.combination.Text2AsciiDoc.create().translate(text));
+		System.out.println(de.vandermeer.skb.interfaces.translators.combination.Text2Html.create().translate(text));
+		System.out.println(de.vandermeer.skb.interfaces.translators.combination.Text2Latex.create().translate(text));
+
+		this.output();
+	}
+
+
+	protected void output(String str){
+		System.out.println(str);
+		System.out.println("----------------------------------------------------------------------------------------");
+	}
+
+	protected void output(){
+		System.out.println("----------------------------------------------------------------------------------------");
+		System.out.println();
+	}
+
+}
